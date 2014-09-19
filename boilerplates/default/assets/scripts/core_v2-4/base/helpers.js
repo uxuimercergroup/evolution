af1da22507e928128d3affdf42f6d94211cb082c
mgholmes <matthew.holmes@mercer.com>
2014-09-19 15:14:01 -0400

    evolution boilerplates for d2-5, need to be completed

---
+++ boilerplates/default/assets/scripts/core_v2-4/base/helpers.js
@@ -0,0 +1,123 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution - Core v2.4 - Helper JS Functions
+// DATE - April 4, 2014
+// AUTHOR - Doug Fraize, Matthew Holmes
+//--------------------------------------------------------------------------------------------------------
+
+// FORM
+//--------------------------------------------------------------------------------------------------------
+
+	// NUMERIC MASK FOR INPUT - Text input only accepts numbers
+	var mulInputNumericMask = function(){
+		jQuery('.js-mul-numeric-mask').on('keypress', function(ev) {
+			var keyCode = window.event ? ev.keyCode : ev.which;
+			//codes for 0-9
+			if (keyCode < 48 || keyCode > 57) {
+			//codes for backspace, delete, enter
+			if (keyCode != 0 && keyCode != 8 && keyCode != 13 && !ev.ctrlKey) {
+				ev.preventDefault();
+				}
+			}
+		});
+	}
+
+	// Input Numeric Mask Init
+	mulInputNumericMask();
+
+	// FORM ELEMENT AUTOFOCUS
+	var mulInputAutofocus = function(){
+		if (!("autofocus" in document.createElement("input"))) {
+			jQuery(".js-mul-autofocus").focus();
+		}
+	}
+
+	// Input Autofocus Init
+	mulInputAutofocus();
+
+
+// INTERACTIVE
+//--------------------------------------------------------------------------------------------------------
+
+	// CHECKBOXES SELECT ALL TOGGLE
+	var mulCheckboxesSelectAllToggle = function(){
+
+		// This checks/unchecks all the checkboxes
+		jQuery('.js-mul-select-all-checkboxes').click(function () {
+			jQuery('.js-mul-select-all-checkbox-item').prop('checked', this.checked);
+		});
+
+		// This is used to when user selects a checkbox in list it will uncheck the check all checkbox
+		jQuery('.js-mul-select-all-checkbox-item').change(function () {
+			var check = (jQuery('.js-mul-select-all-checkbox-item').filter(":checked").length == jQuery('.js-mul-select-all-checkbox-item').length);
+			jQuery('.js-mul-select-all-checkboxes').prop("checked", check);
+		});
+	}
+
+	// Checkboxes Select All Toggle Init
+	mulCheckboxesSelectAllToggle();
+
+	// TOGGLE CONTENT
+	var mulToggleContent = function(){
+		jQuery('.js-mul-toggle-content').click(function(){
+			jQuery('.js-toggle-content-hide').hide(); // Hides all layers first
+			var id = 'div' + jQuery(this).attr('mul-data-toggle');
+			jQuery('#' + id).toggle();
+		});
+	}
+
+	// Toggle Content Init
+	mulToggleContent();
+
+	// TOGGLE CONTENT AND ICON
+	var mulToggleContentIcon = function(){
+		jQuery('.js-mul-toggle-content-icon').click(function(){
+			jQuery('.js-toggle-content-hide').hide(); // Hides all layers first
+			jQuery(this).children('i').toggleClass("mul-icon-minus-circle mul-icon-plus-circle")
+			var id = 'div' + jQuery(this).attr('mul-data-toggle');
+			jQuery('#' + id).toggle();
+		});
+	}
+
+	// Toggle Content and Icon Init
+	mulToggleContentIcon();
+
+	// TIMED FADE OUT EFFECT
+	var mulEffectsFadeOut = function(){
+		setInterval(function(){
+			jQuery(".js-mul-fadeout-element").fadeOut();
+		}, 6500);
+	}
+
+	// Fade Out Effect Init
+	mulEffectsFadeOut();
+
+	// TIMED FADE IN EFFECT
+	var mulEffectsFadeIn = function(){
+		setInterval(function(){
+			jQuery(".js-mul-fadein-element").fadeIn();
+		}, 400);
+	}
+
+	// Fade In Effect Init
+	mulEffectsFadeIn();
+
+
+// LAYOUT
+//--------------------------------------------------------------------------------------------------------
+
+	// SET EQUAL HEIGHTS ON COLUMNS
+	function setEqualHeights(columns){
+		var tallestcolumn = 0;
+		$.each(columns, function(index){
+			var currentHeight = $(columns[index]).height();
+
+			if(currentHeight > tallestcolumn)
+			{
+				tallestcolumn = currentHeight;
+			}
+		});
+
+		$.each(columns, function(index){
+			$(columns[index]).height(tallestcolumn);
+		});
+	};
---
+++ boilerplates/employee-portal/assets/scripts/core_v2-4/components/datepicker.js
@@ -0,0 +1,105 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution - Core v2.4 - Datepicker JS
+// DATE - April 4, 2014
+// AUTHOR - Doug Fraize, Matthew Holmes
+//--------------------------------------------------------------------------------------------------------
+
+// Define datepicker defaults
+var mulDatepickerDefaults = {
+	autoSize: true, // auto-size the date input field for date format choosen
+	buttonText: "Choose Date", // Title text of the button image
+	changeMonth: true,
+	changeYear: true,
+	closeText: "Close", // Set text of close button in button panel
+	currentText: "Today", // Set text of choosing today button in button panel
+	dateFormat: "m/d/yy", // date format: m/d/yyyy - 1/1/2013, not including preceding zeros for single date values
+	minDate: 0, // Dates in the past are disabled and navigation to past calendars is disabled, set to 'null' to allow for dates in the past
+	maxDate: null, // Dates in the future are disabled and navigation to the future is disabled, default is null, refer to jQuery UI Datepicker API documentation for disabing dates in the future (http://api.jqueryui.com/datepicker/#option-maxDate)
+	numberOfMonths: 1, // Show # of months in datepicker
+	showButtonPanel: false, // Show button panel at bottom of datepicker, default is false
+	showOn: "both", // when the datepicker should appear, a value of 'both' means when the input field receives focus or calendar icon is clicked
+	beforeShow: function(input, inst) {
+		
+		// If datepicker input has masked option enabled, change date format to mm/dd/yy.
+		var datepickerMaskedInput = jQuery(this).hasClass("mul-datepicker-masked-input"); // set variable to check if datepicker input has class for masked input option
+		if (datepickerMaskedInput) {
+			jQuery(this).datepicker("option", "dateFormat", "mm/dd/yy").mask("99/99/9999",{placeholder:"_"}); // If masked input option class then mask input for specified format of mm/dd/yyyy in datepicker call
+		};
+		
+		// If datepicker calendar has message option enabled, append to calendar. Gets label from "data-mul-datepicker-message-label" attribute on datepicker input in HTML
+		var datepickerMessageLabel = jQuery(this).attr("data-mul-datepicker-message-label");
+		var appendDatepickerMessage = function () {
+			jQuery("<div class='mul-datepicker-message'><label>" + datepickerMessageLabel + "</label></div>").appendTo(".ui-datepicker");
+		};
+		if (datepickerMessageLabel) {
+			setTimeout(appendDatepickerMessage, 10); // If a datepicker message label is available, show the datepicker message after generating datepicker
+		};
+	},
+	onChangeMonthYear: function () {
+		
+		// If datepicker calendar has message option enabled, append to calendar. Gets label from "data-mul-datepicker-message-label" attribute on datepicker input in HTML
+		var datepickerMessageLabel = jQuery(this).attr("data-mul-datepicker-message-label");
+		var appendDatepickerMessage = function () {
+			jQuery("<div class='mul-datepicker-message'><label>" + datepickerMessageLabel + "</label></div>").appendTo(".ui-datepicker");
+		};
+		if (datepickerMessageLabel) {
+			setTimeout(appendDatepickerMessage, 10); // If a datepicker message label is available, Show the datepicker message after generating datepicker on month/year change
+		};
+	}
+};
+
+// Datepicker function, pass id of datepicker input and any datepicker options you want to override
+var mulDatepicker = function(id, options) {
+	var datepickerId = jQuery("#"+id);
+	datepickerId.datepicker(jQuery.extend({},mulDatepickerDefaults,options));
+};
+
+// Define datepicker date range defaults
+var mulDatepickerRangeDefaults = {
+	autoSize: true, // auto-size the date input field for date format choosen
+	buttonText: "Choose Date", // Title text of the button image
+	changeMonth: true,
+	changeYear: true,
+	closeText: "Close", // Set text of close button in button panel
+	currentText: "Today", // Set text of choosing today button in button panel
+	dateFormat: "m/d/yy", // date format: m/d/yyyy - 1/1/2013, not including preceding zeros for single date values
+	minDate: 0, // Dates in the past are disabled and navigation to past calendars is disabled, set to 'null' to allow for dates in the past
+	maxDate: null, // Dates in the future are disabled and navigation to the future is disabled, default is null, refer to jQuery UI Datepicker API documentation for disabing dates in the future (http://api.jqueryui.com/datepicker/#option-maxDate)
+	numberOfMonths: 1, // Show # of months in datepicker
+	showButtonPanel: false, // Show button panel at bottom of datepicker, default is false
+	showOn: "both", // when the datepicker should appear, a value of 'both' means when the input field receives focus or calendar icon is clicked
+	beforeShow: function(input, inst) {
+		
+		// If datepicker input has masked option enabled, change date format to mm/dd/yy.
+		var datepickerMaskedInput = jQuery(this).hasClass("mul-datepicker-masked-input"); // set variable to check if datepicker input has class for masked input option
+		if (datepickerMaskedInput) {
+			jQuery(this).datepicker("option", "dateFormat", "mm/dd/yy").mask("99/99/9999",{placeholder:"_"}); // If masked input option class then mask input for specified format of mm/dd/yyyy in datepicker call
+		};
+		
+		// If datepicker calendar has message option enabled, append to calendar. Gets label from "data-mul-datepicker-message-label" attribute on datepicker input in HTML
+		var datepickerMessageLabel = jQuery(this).attr("data-mul-datepicker-message-label");
+		var appendDatepickerMessage = function () {
+			jQuery("<div class='mul-datepicker-message'><label>" + datepickerMessageLabel + "</label></div>").appendTo(".ui-datepicker");
+		};
+		if (datepickerMessageLabel) {
+			setTimeout(appendDatepickerMessage, 10); // If a datepicker message label is available, show the datepicker message after generating datepicker
+		};
+	},
+	onChangeMonthYear: function () {
+		
+		// If datepicker calendar has message option enabled, append to calendar. Gets label from "data-mul-datepicker-message-label" attribute on datepicker input in HTML
+		var datepickerMessageLabel = jQuery(this).attr("data-mul-datepicker-message-label");
+		var appendDatepickerMessage = function () {
+			jQuery("<div class='mul-datepicker-message'><label>" + datepickerMessageLabel + "</label></div>").appendTo(".ui-datepicker");
+		};
+		if (datepickerMessageLabel) {
+			setTimeout(appendDatepickerMessage, 10); // If a datepicker message label is available, Show the datepicker message after generating datepicker on month/year change
+		};
+	}
+};
+
+// Datepicker date range function, pass id of datepicker input and any datepicker date range options you want to override
+var mulDatepickerRange = function(id, options) {
+	var datepickerId = jQuery("#"+id);
+	datepickerId.datepicker(jQuery.extend({},mulDatepickerRangeDefaults,options));
+};
