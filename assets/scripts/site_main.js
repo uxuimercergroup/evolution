02f219ec7c0c5e1fdadb0d3b2445795cfc051c35
dfraize <dug.fraize@gmail.com>
2014-10-16 09:11:23 -0400

    More tweaks to the header, pushing towards final version

--- assets/scripts/site_main.js
+++ assets/scripts/site_main.js
@@ -111,13 +111,26 @@ $(function() {
 			jQuery('.js-mul-menus').smartmenus('menuHideAll');
 			jQuery('.js-mul-menus').smartmenus({
 				subIndicators: true,
+				mainMenuSubOffsetX: 0,
+				subMenusSubOffsetX: 5,
+				subMenusSubOffsetY: 0,
+				subMenusMinWidth: '100px',
+				subMenusMaxWidth: '250px',
+				subIndicatorsText: '',
+				subIndicatorsPos: 'append',
+				keepInViewport: true
+			});
+			jQuery('.js-mul-profile-menu').smartmenus({
+				subIndicators: true,
 				mainMenuSubOffsetX: -1,
 				subMenusSubOffsetX: 5,
 				subMenusSubOffsetY: -1,
 				subMenusMinWidth: '100px',
 				subMenusMaxWidth: '250px',
-				subIndicatorsText: '',
-				subIndicatorsPos: 'append'
+				subIndicators: false,
+				showOnClick: true,
+				hideOnClick: true,
+				keepInViewport: true
 			});
 		/* PLEASE CLEAN UP! - ENDS */
 
