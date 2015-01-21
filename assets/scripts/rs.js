7c98882c3d756c8829ac532ca2abd4038e7ff9e2
Matthew Holmes <matthew.holmes@mercer.com>
2015-01-21 18:33:48 -0500

    Red Scout work siloing

---
+++ assets/scripts/rs.js
@@ -0,0 +1,40 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution - Red Scout Edition - Site JS
+// DATE - January 21, 2015
+// AUTHOR - Doug Fraize, Matthew Holmes, Doug Richar, Vinod Bhatt
+//--------------------------------------------------------------------------------------------------------
+
+// =======================================================================================================
+// JAVASCRIPT LIBRARIES
+// =======================================================================================================
+
+
+
+
+// =======================================================================================================
+// SITE FUNCTIONS AND INITIALIZATIONS
+// =======================================================================================================
+
+// FUNCTIONS
+//--------------------------------------------------------------------------------------------------------
+
+	// FIXES FLASH OF UNSTYLED CONTENT BUG - WHERE PAGE LOADS FASTER THAN JS AND MODERNIZR
+	// HIDE PAGE WITH CSS CLASS PROVIDED BY MODERNIZR AND WHEN DOCUMENT IS READY, SHOW PAGE WITH JS
+	var evoFixFOUC = function() {
+		jQuery('html.evo-mod-js').show();
+	}
+
+
+// DOCUMENT READY
+//--------------------------------------------------------------------------------------------------------
+$(function() {
+
+
+	// INITIALIZATIONS
+	//----------------------------------------------------------------------------------------------------
+
+		// FIXES FLASH OF UNSTYLED CONTENT BUG - WHERE PAGE LOADS FASTER THAN JS AND MODERNIZR
+		// HIDE PAGE WITH CSS CLASS PROVIDED BY MODERNIZR AND WHEN DOCUMENT IS READY, SHOW PAGE WITH JS
+		evoFixFOUC();
+
+});
