c80e43f530e8ae4dc698248616e513c35c83a707
Matthew Holmes <matthew.holmes@mercer.com>
2015-02-06 18:37:55 -0500

    fixed code conflict problem, moved js to ep_main.js where it belongs because ep.js is generated js

--- assets/scripts/ep.js
+++ assets/scripts/ep.js
@@ -24,6 +24,22 @@
 		jQuery('html.evo-mod-js').show();
 	}
 
+	// FORM TOGGLE CONTENT
+	var evoFormToggleContent = function(){
+		jQuery('.js-evo-toggle-form-content').focus(function(){
+			var id = 'form-item' + jQuery(this).attr('data-evo-form-toggle');
+			jQuery('#' + id).fadeToggle(2000);
+			return false;
+		});
+	}
+
+	// ITEM SELECTION CLASS TOGGLE
+	var itemSelectionClassToggle = function (){
+		jQuery('.js-evo-category-highlight').click(function(event) {
+			jQuery(this).toggleClass('evo-category-highlight');
+		});
+	}
+
 
 // DOCUMENT READY
 //--------------------------------------------------------------------------------------------------------
@@ -36,5 +52,11 @@ $(function() {
 		// FIXES FLASH OF UNSTYLED CONTENT BUG - WHERE PAGE LOADS FASTER THAN JS AND MODERNIZR
 		// HIDE PAGE WITH CSS CLASS PROVIDED BY MODERNIZR AND WHEN DOCUMENT IS READY, SHOW PAGE WITH JS
 		evoFixFOUC();
+
+		// FORM TOGGLE CONTENT
+		evoFormToggleContent();
+
+		// ITEM SELECTION CLASS TOGGLE
+		itemSelectionClassToggle();
 
 });
