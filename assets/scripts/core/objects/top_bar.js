371c2be24621d89d10c695e76eedc4efd058ed6c
Matthew Holmes <matthew.holmes@mercer.com>
2015-06-11 18:07:54 -0400

    more topbar foundation work for toolbar header

--- assets/scripts/core/objects/top_bar.js
+++ assets/scripts/core/objects/top_bar.js
@@ -7,15 +7,13 @@
 $(document).foundation({
 
 	// TOP BAR
-	/*
 	topbar : {
-		sticky_class : 'sticky',
-		custom_back_text: true, // Set this to false and it will pull the top level link name as the back text
-		back_text: 'Back', // Define what you want your custom back text to be if custom_back_text: true
-		is_hover: true,
-		mobile_show_parent_link: false, // will copy parent links into dropdowns for mobile navigation
-		scrolltop : true // jump to top when sticky nav menu toggle is clicked
+		// sticky_class : 'sticky',
+		// custom_back_text: true, // Set this to false and it will pull the top level link name as the back text
+		// back_text: 'Back', // Define what you want your custom back text to be if custom_back_text: true
+		is_hover: false,
+		mobile_show_parent_link: true // will copy parent links into dropdowns for mobile navigation
+		// scrolltop : true // jump to top when sticky nav menu toggle is clicked
 	}
-	*/
 
 });
