0a86ce551b37b9ad47ea168df51a807b4d557651
Matthew Holmes <matthew.holmes@mercer.com>
2015-09-16 15:36:06 -0400

    evo lib family page updated with new get code model

--- assets/scripts/ee/site.js
+++ assets/scripts/ee/site.js
@@ -46,19 +46,19 @@ $(function() {
 // COMPONENTS
 // =======================================================================================================
 
-// COMPONENT
+// ITEM SELECTION PANEL
 //--------------------------------------------------------------------------------------------------------
-// Mercer Evolution - Core v3.0.1 - Employee Portal Family
+// Mercer Evolution - v3.0.1 - Employee Portal Family
 // Item Selection Panel Component v1.0 - JS
 // DATE - September 1, 2015
 // AUTHOR - Doug Fraize, Matthew Holmes, Sherry Seeton
 //--------------------------------------------------------------------------------------------------------
 
-// ITEM SELECTION PANEL ITEM SELECTED TOGGLE
-var evoItemSelectionPanelItemSelected = function (){
-	jQuery('[data-evo-item-selection-panel-item]').click(function(event) {
-		jQuery(this).toggleClass('evo-item-selection-panel-item-selected');
-	});
-}
-
-evoItemSelectionPanelItemSelected();
+// ITEM SELECTION PANEL ITEM SELECTED TOGGLE - BEGINS
+	var evoItemSelectionPanelItemSelected = function (){
+		jQuery('[data-evo-item-selection-panel-item]').click(function(event) {
+			jQuery(this).toggleClass('evo-item-selection-panel-item-selected');
+		});
+	}
+	evoItemSelectionPanelItemSelected();
+// ITEM SELECTION PANEL ITEM SELECTED TOGGLE - ENDS
