0a1901170370112360cce3b6b732b1bc53aa4119
Doug Fraize <dug.fraize@gmail.com>
2015-10-02 17:35:22 -0400

    bar added

---
+++ templates/ee/components/bar/bar.js
@@ -0,0 +1,10 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution - v3.0.1 - Employee Portal Family
+// Bar Component v1.0 - JS
+// DATE - September 1, 2015
+// AUTHOR - Doug Fraize, Matthew Holmes, Sherry Seeton
+//--------------------------------------------------------------------------------------------------------
+
+// BAR COMPONENT - BEGINS
+
+// BAR COMPONENT - ENDS
