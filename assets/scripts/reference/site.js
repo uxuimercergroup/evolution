a59b4b89782c9cc0e033dae15242e5fd14505a1f
Matthew Holmes <matthew.holmes@mercer.com>
2015-10-06 18:02:22 -0400

    conversational form work for #20

--- assets/scripts/reference/site.js
+++ assets/scripts/reference/site.js
@@ -282,19 +282,15 @@ $(function() {
 // AUTHOR - Doug Fraize, Matthew Holmes, Sherry Seeton
 //--------------------------------------------------------------------------------------------------------
 
-// CONVERSATIONAL FORM COMPONENT - BEGINS
-
-// FORM TOGGLE CONTENT
-	var evoFormToggleContent = function(){
-		jQuery('.js-evo-toggle-form-content').focus(function(){
-			var id = 'form-item' + jQuery(this).attr('data-evo-form-toggle');
+// CONVERSATIONAL FORM ITEM TOGGLE  - BEGINS
+	var evoConversationalFormItemToggle = function(){
+		jQuery('[data-evo-conversational-form-element]').focus(function(){
+			var id = 'evo-conversational-form-item-' + jQuery(this).attr('data-evo-conversational-form-item-toggle');
 			jQuery('#' + id).fadeIn(2000);
-			return false;
 		});
 	}
-	evoFormToggleContent();
-	
-// CONVERSATIONAL FORM COMPONENT - ENDS
+	evoConversationalFormItemToggle();
+// CONVERSATIONAL FORM ITEM TOGGLE - ENDS
 
 // ITEM SELECTION PANEL
 //--------------------------------------------------------------------------------------------------------
