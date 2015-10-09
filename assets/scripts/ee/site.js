36b76c6c2dfb1d66aa07c59f3e932503f9d51203
Matthew Holmes <matthew.holmes@mercer.com>
2015-10-09 18:39:52 -0400

    slider and buttons added to ee component page

--- assets/scripts/ee/site.js
+++ assets/scripts/ee/site.js
@@ -37,8 +37,39 @@ $(function() {
 	// INITIALIZATIONS
 	//----------------------------------------------------------------------------------------------------
 
-	
 		
+
+	
+	// LIBRARY EXAMPLES
+	//----------------------------------------------------------------------------------------------------
+
+		// SLIDER
+
+			// Slider horizontal (default) with interval ticks example
+			evoSlider('evo-slider-default-ticks',{
+				step: 10,
+				ticks: true
+			});
+
+			// Slider range horizontal (default) with interval ticks example
+			evoSliderRange('evo-slider-range-default-ticks',{
+				step: 10,
+				ticks: true
+			});
+
+			// Slider range horizontal - fixed min with interval ticks example
+			evoSliderRangeSingleHandle('evo-slider-range-single-min-ticks',{
+				range: 'min',
+				step: 10,
+				ticks: true
+			});
+
+			// Slider range horizontal - fixed max with interval ticks example
+			evoSliderRangeSingleHandle('evo-slider-range-single-max-ticks',{
+				range: 'max',
+				step: 10,
+				ticks: true
+			});
 
 });
 
